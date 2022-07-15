e3ef80caf307379dc75ecb409ae5313d5eea5af8
Admin <admin>
2022-07-15 17:45:08 +0300

    Add static resources

---
+++ data/src/main/java/petclinic/service/map/VetServiceMap.java
@@ -0,0 +1,35 @@
+package petclinic.service.map;
+
+import org.springframework.stereotype.Service;
+import petclinic.model.Vet;
+import petclinic.service.VetService;
+
+import java.util.Set;
+
+@Service
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
