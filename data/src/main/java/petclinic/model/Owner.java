70a098a6dbc2be2c7744dadce1bd524f0d906ce4
Olga <pin007@tut.by>
2022-07-24 20:48:58 +0300

    Add Lombok

--- data/src/main/java/petclinic/model/Owner.java
+++ data/src/main/java/petclinic/model/Owner.java
@@ -1,6 +1,8 @@
 package petclinic.model;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,9 +11,11 @@ import java.util.Set;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
+
 
     @Column(name = "address")
     private String address;
@@ -25,4 +29,14 @@ public class Owner extends Person {
     @OneToMany(cascade = CascadeType.ALL,
             mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
+
+    @Builder
+    public Owner(Long id, String firstName, String lastName,
+                 String address, String city, String telephone, Set<Pet> pets) {
+        super(id, firstName, lastName);
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
+        this.pets = pets;
+    }
 }
