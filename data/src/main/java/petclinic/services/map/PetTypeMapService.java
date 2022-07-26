65f35dc532c064a8b9ff1154dbcc81ef146ab257
Olga <pin007@tut.by>
2022-07-26 21:08:14 +0300

    JUnit test for OwnerMapService

--- data/src/main/java/petclinic/services/map/PetTypeMapService.java
+++ data/src/main/java/petclinic/services/map/PetTypeMapService.java
@@ -3,46 +3,36 @@ package petclinic.services.map;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import petclinic.model.PetType;
-import petclinic.repositories.PetTypeRepository;
 import petclinic.services.PetTypeService;
 
-import java.util.HashSet;
 import java.util.Set;
 
 @Service
 @Profile({"default", "map"})
 public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
 
-    private final PetTypeRepository petTypeRepository;
-
-    public PetTypeMapService(PetTypeRepository petTypeRepository) {
-        this.petTypeRepository = petTypeRepository;
-    }
-
     @Override
     public Set<PetType> findAll() {
-        Set<PetType> petTypes = new HashSet<>();
-        petTypeRepository.findAll().forEach(petTypes::add);
-        return petTypes;
+        return super.findAll();
     }
 
     @Override
-    public void delete(PetType object) {
-        petTypeRepository.delete(object);
-    }
-
-    @Override
-    public PetType findById(Long aLong) {
-        return petTypeRepository.findById(aLong).orElse(null);
+    public PetType findById(Long id) {
+        return super.findById(id);
     }
 
     @Override
     public PetType save(PetType object) {
-        return petTypeRepository.save(object);
+        return super.save(object);
     }
 
     @Override
-    public void deleteById(Long aLong) {
-        petTypeRepository.deleteById(aLong);
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
     }
 }
